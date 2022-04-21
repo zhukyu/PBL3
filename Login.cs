e1c325b6756b194d8ac4d5ef29383d627801768e
Le van thanh <102200356@sv1.dut.udn.vn>
2022-04-21 10:57:07 +0700

    them login

--- Login.cs
+++ Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Gym
 {
@@ -34,10 +35,53 @@ namespace Gym
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            var homeForm = new Home();
-            this.Hide();
-            homeForm.ShowDialog();
-            this.Close();
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-S7H27N8\SQLEXPRESS;Initial Catalog=Gymmer;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk = username.Text;
+                string mk = password.Text;
+                string sql = "select *from NguoiDung where taikhoan='" + tk + "'and matkhau='" + mk + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader data = (SqlDataReader)cmd.ExecuteReader();
+
+
+                if (username.Text == "")
+                {
+                    MessageBox.Show("ban chua nhap uername");
+                }
+                if (password.Text == "")
+                {
+                    MessageBox.Show("ban chua nhap password");
+                }
+
+                if (username.Text != "" && password.Text != "")
+                    if (data.Read() == true)
+                    {
+                        MessageBox.Show("dang nhap thanh cong");
+                        var homeform = new Home();
+                        this.Hide();
+                        homeform.Show();
+                        // this.Close();
+
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("dang nhap that bai");
+                    }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("loi ket noi");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
